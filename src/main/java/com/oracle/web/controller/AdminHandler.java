0a07b82b1315db621299a1571053b289215990eb
YUbin <2364058959@qq.com>
2019-05-17 15:54:54 +0800

    YuBin提交了代码

--- src/main/java/com/oracle/web/controller/AdminHandler.java
+++ src/main/java/com/oracle/web/controller/AdminHandler.java
@@ -1,5 +1,13 @@
 package com.oracle.web.controller;
 
-public class AdminHandler {
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.oracle.web.service.AdminService;
+
+public class AdminHandler {
+	
+	private static final String NONE = null;
+	@Autowired
+	private AdminService adminService;
 }
+
