906f7833d46bc59d153db124ea2b3e968053e406
YUbin <2364058959@qq.com>
2019-05-17 16:18:31 +0800

    余彬又提交了一次

--- src/main/java/com/oracle/web/service/impl/AdminServiceImpl.java
+++ src/main/java/com/oracle/web/service/impl/AdminServiceImpl.java
@@ -1,14 +1,17 @@
 package com.oracle.web.service.impl;
 
+import com.oracle.mapper.AdminMapper;
 import com.oracle.web.bean.Admin;
 import com.oracle.web.service.AdminService;
 
 public class AdminServiceImpl implements AdminService {
 
+	private AdminMapper adminMapper;
+
 	@Override
 	public int save(Admin admin) {
 		// TODO Auto-generated method stub
-		return 0;
+		return this.adminMapper.save(admin);
 	}
 
 }
